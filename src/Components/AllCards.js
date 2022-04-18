4fbf477d4ed32cb95cb4d2b346ccf50a006a981f
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2022-04-18 15:47:30 +0530

    search by name added.

--- src/Components/AllCards.js
+++ src/Components/AllCards.js
@@ -11,7 +11,8 @@ class AllCards extends Component{
         this.state={
             cards : [],
             errorMessage : "Please Wait...",
-            successMessage : ""
+            successMessage : "",
+            search : "",
         }
     }
 
@@ -41,26 +42,54 @@ class AllCards extends Component{
                 
             });
     } 
+    handlechange = event => {
+        
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        console.log(name,value);
+        const {formValue} = this.state;
+        this.setState({
+            search:value})
+        
+    };
 
     render(){
-
-        var cards = this.state.cards;
-        if(cards.length > 0){
-            return(
+        var searchCards = [];
+        if(this.state.search == ""){
+            searchCards = this.state.cards;
+        }
+        else{
+            for (let i = 0, len = this.state.cards.length; i < len; i++) {
+                var s = this.state.cards[i].name.toLocaleUpperCase();
+                if(s.match(this.state.search.toLocaleUpperCase())){
+                    searchCards.push(this.state.cards[i]);
+                }
+              }
+        }
+        
+        return(
+            <>
+                <label class="sr-only" for="inlineFormInputGroupUsername2">Card Name</label>
+                <div class="input-group col-md-3 col-sm-12" style={{margin:10}} >
+                <div class="input-group-prepend">
+                    <div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg></div>
+                </div>
+                <input type="text" class="form-control col-md-12 col-sm-12" id="inlineFormInputGroupUsername2" placeholder="Card Name" onChange={this.handlechange}/>
+                </div>
                 <CardGroup className="row justify-content-between" style = {{marginLeft: 10}}>
                     {
-                        cards.map((item, index) => (
+                        searchCards.map((item, index) => (
                             <CreateCard key = {index} package = {item} index = {index}></CreateCard>
                         ))
                     }
                 </CardGroup>
-            );
-        }
-        return(
-            <div class="alert alert-primary" role="alert" style={{width:"100%", textAlign: "center"}}>
-                {this.state.errorMessage}
-            </div>   
-        )
+            </>
+        );
+        
+        
 
     }
 }
