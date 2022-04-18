940110fa5eb40b0c97f4d5fbbde16322ce9150a1
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2022-04-18 14:54:03 +0530

    url updated

--- src/Components/BackendURL.js
+++ src/Components/BackendURL.js
@@ -1,3 +1,3 @@
 export const backendUrlFindAllCards = "https://volopay-backend.herokuapp.com/volopay_Server/getCard/allcard/";
 export const backendUrlFindAllOwners = "https://volopay-backend.herokuapp.com/volopay_Server/getCard/allowners/";
-export const backendUrlFindByOwners = "http://volopay-backend.herokuapp.com/volopay_Server/getCard/userCards/";
+export const backendUrlFindByOwners = "https://volopay-backend.herokuapp.com/volopay_Server/getCard/userCards/";
